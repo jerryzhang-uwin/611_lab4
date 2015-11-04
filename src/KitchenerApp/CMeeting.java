70e6556925e4d126658913ad2ab743828d92e20f
jerryzhang-uwin <jerryzhang.uwin@yahoo.com>
2015-11-03 23:29:17 -0500

    Added Class CMeeting and its JUnit Test Class.

---
+++ src/KitchenerApp/CMeeting.java
@@ -0,0 +1,5 @@
+package KitchenerApp;
+
+public class CMeeting {
+
+}
---
+++ src/KitchenerAppTests/TestCMeeting.java
@@ -0,0 +1,14 @@
+package KitchenerAppTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCMeeting {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
